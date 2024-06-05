3a9f596597f991025babbcaa3efc7a1a2fee93a6
PavelVorobyev77 <pascha27_05@mail.ru>
2024-06-05 20:17:21 +0700

    почти итог

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -32,6 +34,7 @@ namespace CalculatorVP
                 }
             }
         }
+        private double memoryValue = 0;
 
         private void ButtonClick(Object sender, RoutedEventArgs e)
         {
@@ -41,22 +44,59 @@ namespace CalculatorVP
                 if (textButton == "C")
                 {
                     text.Clear();
+                    memoryValue = 0;
                 }
                 else if (textButton == "x")
                 {
-                    text.Text = text.Text.Substring(text.Text.Length - 1);
+                    if (text.Text.Length > 0)
+                    {
+                        text.Text = text.Text.Substring(0, text.Text.Length - 1);
+                    }
                 }
                 else if (textButton == "=")
                 {
-                    text.Text = new DataTable().Compute(text.Text, null).ToString();
+                    string expression = text.Text.Replace(",", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator);
+                    object result = new DataTable().Compute(expression, null);
+                    text.Text = result.ToString().Replace(CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator, ",");
                 }
-                else text.Text += textButton;
+
+                else if (textButton == "MR")
+                {
+                    text.Text = memoryValue.ToString();
+                }
+                else if (textButton == "M+")
+                {
+                    memoryValue += double.Parse(text.Text);
+                    text.Clear();
+                }
+                else if (textButton == "MC")
+                {
+                    memoryValue = 0;
+                }
+                else if (textButton == "%")
+                {
+                    if (text.Text.Contains("%"))
+                    {
+                        string[] values = text.Text.Split('%');
+                        double value1 = double.Parse(values[0]);
+                        double value2 = double.Parse(values[1]);
+                        text.Text = (value1 % value2).ToString();
+                    }
+                    else
+                    {
+                        text.Text += "%";
+                    }
+                }
+
+                else
+                {
+                    text.Text += textButton;
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-
         }
 
     }
